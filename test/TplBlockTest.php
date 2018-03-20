a121d1974dfad754b44b055133f77a24e1307850
Gnieark <gnieark@free.fr>
2018-03-20 19:03:06 +0100

    improve unit tests

--- test/TplBlockTest.php
+++ test/TplBlockTest.php
@@ -23,5 +23,36 @@ class TplBlockTest extends TestCase{
           );
         $this->assertEquals("Hello Gnieark", $tpl->apply_tpl_str("Hello {{name}}"));    
     }
+    //test from a file
+    public function testParseFromFile(){
+        file_put_contents("temp.txt","Hello {{name}}");
+        $tpl = new TplBlock();
+        $tpl->add_vars(array(
+            "name" => "Gnieark",
+            "title" => "Monsieur",
+            "firstname" => "Grouik"
+            )
+          );
+          $this->assertEquals("Hello Gnieark", $tpl->apply_tpl_file("temp.txt"));
+    }
+
+    //test blocs
+    public function testBlocs(){
+        $str = "
+            Bhah blah wpooie456
+            <!-- BEGIN bloc -->
+                have to be shown
+            <!-- END bloc -->
+            <!-- BEGIN blocTwo -->
+                WONT to be shown
+            <!-- END blocTwo -->
+        ";
+        $tpl = new TplBlock();
+        $tpl2 = new TplBlock("bloc");
+        $tpl->add_sub_block($tpl2);
+        $str = $tpl->apply_tpl_str($str);
+        $this->assertContains('have',$str);
+        $this->assertFalse(strpos("WONT",$str));
+    }
 
 }
